2be52be94cf5d9d443e0ceb2d9dab8dd2ead20ee
Nevatia <nevan01@ca.com>
2018-09-10 13:53:52 +0530

    MaxChar, ReverseString

--- src/Prac/ReverseString.js
+++ src/Prac/ReverseString.js
@@ -1,1 +1,14 @@
-console.log("Testing");
+function reverse (str) {
+    let revStr = "";
+    for (let char of str) {
+        revStr = char + revStr;
+    }
+    return revStr;
+}
+
+function reverse1 (str) {
+    return str.split('').reduce((rev, char) => char + rev);
+}
+
+console.log(reverse("apple"));
+console.log(reverse1("practice"));
